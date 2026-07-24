41f95305fcaf44f68ebabb5fdb91edd19fa3146b
Rocio <rochioveralopez@gmail,com>
2026-07-24 17:24:50 -0300

    Se agrega resolucion de consulta 3 y 4

--- m5_consultas_join.sql
+++ m5_consultas_join.sql
@@ -32,3 +32,47 @@ FROM clientes AS c
 LEFT JOIN ventas AS v
 ON c.id_cliente = v.id_cliente
 WHERE v.id_venta IS NULL;
+
+--Consulta 3 — Productos sin ventas (LEFT JOIN) 
+SELECT	
+	 p.nombre_producto,
+	 c.nombre_categoria,
+	 p.precio
+FROM productos AS p
+LEFT JOIN ventas AS v
+ON p.id_producto = v.id_producto
+
+INNER JOIN categorias AS c
+ON p.id_categoria  = c.id_categoria
+WHERE v.id_venta IS NULL;
+
+-- =========================================================================
+-- Consulta 4 — Consolidado por canal (UNION ALL)
+-- NOTA: Dado que la BD Ventas_Tech_DB no posee tablas separadas 
+-- ni una columna 'canal', se simula la división de canales (Online/Presencial) 
+-- utilizando el id_venta para poder demostrar el uso correcto de UNION ALL y GROUP BY.
+-- =========================================================================
+
+SELECT 
+    canal, 
+    SUM(total_venta) AS total_por_canal
+FROM (
+    -- Simulamos las ventas ONLINE (IDs del 1 al 5)
+    SELECT 
+        id_venta, 
+        (cantidad * precio_unitario) AS total_venta, 
+        'Online' AS canal
+    FROM ventas
+    WHERE id_venta <= 5
+
+    UNION ALL
+
+    -- Simulamos las ventas PRESENCIALES (IDs mayores a 5)
+    SELECT 
+        id_venta, 
+        (cantidad * precio_unitario) AS total_venta, 
+        'Presencial' AS canal
+    FROM ventas
+    WHERE id_venta > 5
+) AS consolidado_ventas
+GROUP BY canal;
